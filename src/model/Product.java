43aa92faa718076dece2db7b9fa866b5d696ee10
Sindhusrinivas <srinivasprasad.s@northeastern.edu>
2022-02-04 22:23:13 -0500

    first assignment

---
+++ src/model/Product.java
@@ -0,0 +1,166 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author sindhusrinivas
+ */
+public class Product {
+    
+    
+    private String brand;
+    private String model;
+    private String color;
+    private String year;
+    private String enginenumber;
+    private String seatsnumber;
+    private String licenseplates;
+    private String ownername;
+    private String ownertelephone;
+    private String owneremail;
+    private String ownerdrivinglicense;
+    private String ssn;
+    private String owneraddress;
+    private String servicerecords;
+    private String warrantyyear;
+    private BufferedImage photo;
+
+    public BufferedImage getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(BufferedImage photo) {
+        this.photo = photo;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getEnginenumber() {
+        return enginenumber;
+    }
+
+    public void setEnginenumber(String enginenumber) {
+        this.enginenumber = enginenumber;
+    }
+
+    public String getSeatsnumber() {
+        return seatsnumber;
+    }
+
+    public void setSeatsnumber(String seatsnumber) {
+        this.seatsnumber = seatsnumber;
+    }
+
+    public String getLicenseplates() {
+        return licenseplates;
+    }
+
+    public void setLicenseplates(String licenseplates) {
+        this.licenseplates = licenseplates;
+    }
+
+    public String getOwnername() {
+        return ownername;
+    }
+
+    public void setOwnername(String ownername) {
+        this.ownername = ownername;
+    }
+
+    public String getOwnertelephone() {
+        return ownertelephone;
+    }
+
+    public void setOwnertelephone(String ownertelephone) {
+        this.ownertelephone = ownertelephone;
+    }
+
+    public String getOwneremail() {
+        return owneremail;
+    }
+
+    public void setOwneremail(String owneremail) {
+        this.owneremail = owneremail;
+    }
+
+    public String getOwnerdrivinglicense() {
+        return ownerdrivinglicense;
+    }
+
+    public void setOwnerdrivinglicense(String ownerdrivinglicense) {
+        this.ownerdrivinglicense = ownerdrivinglicense;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getOwneraddress() {
+        return owneraddress;
+    }
+
+    public void setOwneraddress(String owneraddress) {
+        this.owneraddress = owneraddress;
+    }
+
+    public String getServicerecords() {
+        return servicerecords;
+    }
+
+    public void setServicerecords(String servicerecords) {
+        this.servicerecords = servicerecords;
+    }
+
+    public String getWarrantyyear() {
+        return warrantyyear;
+    }
+
+    public void setWarrantyyear(String warrantyyear) {
+        this.warrantyyear = warrantyyear;
+    }
+    
+    
+   
+}
+
+    
+
